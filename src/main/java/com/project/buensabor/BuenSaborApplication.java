d12b37c536a1e1f74c0b54749ddc446ff415fe6b
Rxvargas92 <rxvargas.92@gmail.com>
2023-03-09 19:03:05 -0300

    Creacion del proyecto y estructura de las carpetas

---
+++ src/main/java/com/project/buensabor/BuenSaborApplication.java
@@ -0,0 +1,13 @@
+package com.project.buensabor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuenSaborApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuenSaborApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/buensabor/BuenSaborApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.buensabor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuenSaborApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

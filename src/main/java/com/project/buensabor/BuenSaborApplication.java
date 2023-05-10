f77f50a2e62135a33844aacbc1f3aa7d7a5290be
Rxvargas92 <rxvargas.92@gmail.com>
2023-05-10 11:25:32 -0300

    variables de entorno

--- src/main/java/com/project/buensabor/BuenSaborApplication.java
+++ src/main/java/com/project/buensabor/BuenSaborApplication.java
@@ -1,5 +1,7 @@
 package com.project.buensabor;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import io.github.cdimascio.dotenv.DotenvBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -10,6 +12,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class BuenSaborApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().directory("D:\\Rodri Drive\\Facultad\\Facu Programacion\\2do año\\4to semestre\\Repositorio Buen Sabor\\Buen-Sabor\\").filename("entorno.env").load();
+
+
 		SpringApplication.run(BuenSaborApplication.class, args
 		);
 	}

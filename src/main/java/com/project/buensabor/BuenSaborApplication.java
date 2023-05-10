c18c546ca2eb9ea0beed6d2e00785bcaee880dfa
Rxvargas92 <rxvargas.92@gmail.com>
2023-05-10 15:28:49 -0300

    .

--- src/main/java/com/project/buensabor/BuenSaborApplication.java
+++ src/main/java/com/project/buensabor/BuenSaborApplication.java
@@ -12,8 +12,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class BuenSaborApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.configure().directory("D:\\Rodri Drive\\Facultad\\Facu Programacion\\2do año\\4to semestre\\Repositorio Buen Sabor\\Buen-Sabor\\").filename("entorno.env").load();
-
 
 		SpringApplication.run(BuenSaborApplication.class, args
 		);

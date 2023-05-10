794a697f0190ed5ee24ca72f28d0a1d6fda4c594
Rxvargas92 <rxvargas.92@gmail.com>
2023-05-10 17:03:17 -0300

    .

--- src/main/java/com/project/buensabor/BuenSaborApplication.java
+++ src/main/java/com/project/buensabor/BuenSaborApplication.java
@@ -1,12 +1,9 @@
 package com.project.buensabor;
 
-import io.github.cdimascio.dotenv.Dotenv;
-import io.github.cdimascio.dotenv.DotenvBuilder;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @SpringBootApplication
 public class BuenSaborApplication {

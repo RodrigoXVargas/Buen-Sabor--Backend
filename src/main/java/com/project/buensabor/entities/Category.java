9a9332dc84a69b4654ed4315ae75365c3d4f0225
Rxvargas92 <rxvargas.92@gmail.com>
2023-05-12 22:38:52 -0300

    solucion a seguridad y metodo de prueba para categoria

--- src/main/java/com/project/buensabor/entities/Category.java
+++ src/main/java/com/project/buensabor/entities/Category.java
@@ -8,6 +8,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -33,5 +34,20 @@ public class Category extends Base {
     @OneToMany(mappedBy = "subcategory", cascade = CascadeType.ALL, orphanRemoval = true)
     @JsonManagedReference(value = "subcategory-products")
     private List<Product> products;
+
+    public List<Category> getCategories(){
+        List<Category> categories = new ArrayList<>();
+        getArbolCategorias(this,categories);
+        return categories;
+    }
+
+    public void getArbolCategorias(Category category, List<Category> categorias){
+        categorias.add(category);
+        if(category.getSubcategories()!=null){
+            for (Category categ : category.getSubcategories()) {
+                getArbolCategorias(categ, categorias);
+            }
+        }
+    }
 
 }

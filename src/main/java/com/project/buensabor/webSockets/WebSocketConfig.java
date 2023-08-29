e6ccf05ec433e8bf4156c5db20eee7838863c209
Rxvargas92 <rxvargas.92@gmail.com>
2023-08-29 20:56:50 -0300

    configuro cors para nuevos puertos del front

--- src/main/java/com/project/buensabor/webSockets/WebSocketConfig.java
+++ src/main/java/com/project/buensabor/webSockets/WebSocketConfig.java
@@ -19,7 +19,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("http://localhost:3000", "http://localhost:5173")
+                .setAllowedOrigins("http://localhost:3000", "http://localhost:5173", "http://localhost:4200")
                 .withSockJS();
     }
 }
